740babfbf468419e388ac529d63b103cb6211f68
Makoy <neimarkbraga@gmail.com>
2019-06-15 11:29:50 +0800

    fixed ownership contract transfer function

--- smart-contracts/contracts/CampaignOwnership.sol
+++ smart-contracts/contracts/CampaignOwnership.sol
@@ -15,13 +15,6 @@ contract CampaignOwnership is CampaignHelper, ERC721 {
         return campaignToOwner[_tokenId];
     }
 
-    function _transfer(address _from, address _to, uint256 _tokenId) private {
-        ownerCampaignCount[_to] = ownerCampaignCount[_to].add(1);
-        ownerCampaignCount[_from] = ownerCampaignCount[_from].sub(1);
-        campaignToOwner[_tokenId] = _to;
-        emit Transfer(_from, _to, _tokenId);
-    }
-
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
         require (campaignToOwner[_tokenId] == msg.sender || campaignApprovals[_tokenId] == msg.sender);
         _transfer(_from, _to, _tokenId);
@@ -30,5 +23,23 @@ contract CampaignOwnership is CampaignHelper, ERC721 {
     function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
         campaignApprovals[_tokenId] = _approved;
         emit Approval(msg.sender, _approved, _tokenId);
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        require(campaignToOwner[_tokenId] == _from, "ERC721: transfer of token that is not own");
+        require(_to != address(0), "ERC721: transfer to the zero address");
+
+        _clearApproval(_tokenId);
+
+        ownerCampaignCount[_to] = ownerCampaignCount[_to].add(1);
+        ownerCampaignCount[_from] = ownerCampaignCount[_from].sub(1);
+        campaignToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function _clearApproval(uint256 tokenId) private {
+        if (campaignApprovals[tokenId] != address(0)) {
+            campaignApprovals[tokenId] = address(0);
+        }
     }
 }
