2670abd2369831639c65def330f70426012fb9f9
Makoy <neimarkbraga@gmail.com>
2019-06-15 12:03:27 +0800

    added tests for platform donations

--- smart-contracts/test/FundingTest.js
+++ smart-contracts/test/FundingTest.js
@@ -1,4 +1,4 @@
-const CampaignOwnership = artifacts.require('CampaignOwnership');
+const FundraisingPlatform = artifacts.require('FundraisingPlatform');
 const { increaseTime } = require('./utils');
 
 const DAY = 3600 * 12;
@@ -8,7 +8,7 @@ contract('Fundraising Platform', accounts => {
     let _contract;
 
     beforeEach(async() => {
-        _contract = await CampaignOwnership.new();
+        _contract = await FundraisingPlatform.new();
     });
 
     it('The contract should start with no campaigns.', async() => {
@@ -244,6 +244,49 @@ contract('Fundraising Platform', accounts => {
         try {
             await _contract.createCampaign('0x4e', '0x4e', '0x4e', 100, DAY, {from: firstAccount});
             await _contract.transferFrom(firstAccount, secondAccount, 0, {from: thirdAccount});
+            assert.fail();
+        }
+        catch(error) {
+            assert.ok(/revert/.test(error.message));
+        }
+    });
+
+    it('Does accept donation to platform and allows platform owner to see received donations', async() => {
+        _contract = await FundraisingPlatform.new({ from: firstAccount });
+        await _contract.donateToPlatform({from: firstAccount, value: 100});
+        await _contract.donateToPlatform({from: secondAccount, value: 100});
+        let total_received = (await _contract.getReceivedDonation({from: firstAccount})).toNumber();
+        assert.equal(total_received, 200);
+    });
+
+    it('Does not allow non-owner of platform to see received donations', async() => {
+        try {
+            _contract = await FundraisingPlatform.new({ from: firstAccount });
+            await _contract.donateToPlatform({from: firstAccount, value: 100});
+            await _contract.donateToPlatform({from: secondAccount, value: 100});
+            await _contract.getReceivedDonation({from: secondAccount});
+            assert.fail();
+        }
+        catch(error) {
+            assert.ok(/revert/.test(error.message));
+        }
+    });
+
+    it('Does allow platform owner to withdraw received donations', async() => {
+        _contract = await FundraisingPlatform.new({ from: firstAccount });
+        await _contract.donateToPlatform({from: firstAccount, value: 100});
+        await _contract.donateToPlatform({from: secondAccount, value: 100});
+        await _contract.withdrawReceivedDonation({from: firstAccount});
+        let balance = (await _contract.getReceivedDonation({from: firstAccount})).toNumber();
+        assert.equal(balance, 0);
+    });
+
+    it('Does not allow non-owner of platform to withdraw received donations', async() => {
+        try {
+            _contract = await FundraisingPlatform.new({ from: firstAccount });
+            await _contract.donateToPlatform({from: firstAccount, value: 100});
+            await _contract.donateToPlatform({from: secondAccount, value: 100});
+            await _contract.withdrawReceivedDonation({from: secondAccount});
             assert.fail();
         }
         catch(error) {
